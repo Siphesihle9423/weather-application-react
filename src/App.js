959cd500700355c25e0cf4056d11f0db0b2743f4
Siphesihle9423 <s.s.s.dlamini9423@gmail.com>
2024-08-19 13:19:48 +0200

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      This project was coded by <a href="https://github.com/" target="_blank"> 
+      Siphesihle Dlamini 
+      </a>{" "} 
+      and is{" "}
+  <a href="https://github.com/Siphesihle9423/weather-application-react" target="_blank">
+   open-sourced on GitHub
+  </a>
+  </footer>
+  </div>
   );
 }
-
-export default App;
