fc2e18d26e8f60e970f5a8577652998756319ef2
Siphesihle9423 <s.s.s.dlamini9423@gmail.com>
2024-08-20 13:09:59 +0200

    Removed target blank from URL

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ export default function App() {
   <div className="App">
     <h1>Weather App</h1>
     <footer>
-      This project was coded by <a href="https://github.com/" target="_blank"> 
+      This project was coded by <a href="https://github.com/" > 
       Siphesihle Dlamini 
       </a>{" "} 
       and is{" "}
-  <a href="https://github.com/Siphesihle9423/weather-application-react" target="_blank">
+  <a href="https://github.com/Siphesihle9423/weather-application-react" >
    open-sourced on GitHub
   </a>
   </footer>
